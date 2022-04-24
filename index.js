e67479f973bd25fa9fd15e779347bbd45e0f1546
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-04-24 10:12:37 -0400

    commit tree 1ac64422d76e666e2a83d36621f4349bb563c4f0

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    updateDOM();
+  });
+
+function updateDOM() {
+    console.log("updateDOM function triggered")
+    document.getElementById("text").innerHTML = "This is really cool!";
+}
